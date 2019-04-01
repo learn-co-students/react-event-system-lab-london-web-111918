2cd06e8bd65de18c526b81ddc8df9833f477d808
Edward Sparks <edd_sparks@hotmail.com>
2019-04-01 16:53:41 +0100

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,12 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+// Code EyesOnMe Component Here
+const EyesOnMe = ({props}) => {
+  return(
+    <button onBlur={() => console.log('Hey! Eyes on me!')} onFocus={() => console.log('Good!')}></button>
+    )
+}
+
+
+export default EyesOnMe
+
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
+import React from 'react'
 // Code Keypad Component Here
+const Keypad = ({props}) => {
+  return(
+    <input type="password" onKeyUp={() => console.log('Entering password...')}/>
+    )
+}
+
+
+export default Keypad
+
